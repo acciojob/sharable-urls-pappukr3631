5bab28230620acab9b8149e7b77eb940bd43c3db
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-08-15 21:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+const url = document.getElementById("url");
+
+function process() {
+	url.innerText = "https://localhost:8080/";
+	
+	const name = document.getElementById("name");
+	const year = document.getElementById("year");
+	if(name.value == "" && year.value == "")
+		return;
+	
+	url.innerText += "?";
+	if(name.value != "") {
+		url.innerText += "name=" + name.value;
+		if(year.value != "")
+			url.innerText += "&";
+	}
+
+	if(year.value != "") {
+		url.innerText += "year=" + year.value;
+	}
+}
